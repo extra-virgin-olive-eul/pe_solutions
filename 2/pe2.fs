43af08d76883de85e26ca14c713df7756de673e3
joe <joseph.dougherty@gmail.com>
2017-10-14 02:41:17 -0400

    Adds Forth solution for #2g

---
+++ 2/pe2.fs
@@ -0,0 +1,12 @@
+: nextfib ( -- ) over over + rot drop ;
+: lessthan4mil ( -- ) dup 4000000 < IF true ELSE false ENDIF ;
+: even ( -- ) dup 2 mod 0= IF true ELSE false ENDIF ;
+
+variable v
+0 v !
+
+: main ( -- ) BEGIN nextfib lessthan4mil WHILE even IF dup v @ + v ! ENDIF REPEAT ;
+
+1 1 
+main 
+v @ .
