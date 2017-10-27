fd8397069e45f0f218664d9a0880d011de96fdbd
joe <joseph.dougherty@gmail.com>
2017-10-27 02:52:53 -0400

    Move naive_one_liner to the correct directory

--- 1/pe1.fs
+++ 1/pe1.fs
@@ -1,7 +1,8 @@
-: mod3 ( -- ) 3 mod 0= if true else false endif ;
-: mod5 ( -- ) 5 mod 0= if true else false endif ;
-: mod3or5 ( -- ) dup mod3 true = if true else dup mod5 endif ;
-: main ( -- ) 1000 1 ?DO i mod3or5 true = if + else drop endif LOOP ;
+: mod3 ( -- ) 3 mod 0= IF true ELSE false ENDIF ;
+: mod5 ( -- ) 5 mod 0= IF true ELSE false ENDIF ;
+: mod3or5 ( -- ) dup mod3 true = IF true ELSE dup mod5 ENDIF ;
+: main ( -- ) 1000 1 ?DO i mod3or5 true = IF + ELSE drop ENDIF LOOP ;
 
 0
 main
+.
--- 2/pe2.fs
+++ 2/pe2.fs
@@ -1,11 +1,11 @@
-: nextfib ( -- ) over over + rot drop ;
-: lessthan4mil ( -- ) dup 4000000 < IF true ELSE false ENDIF ;
-: even ( -- ) dup 2 mod 0= IF true ELSE false ENDIF ;
-
 variable v
 0 v !
 
-: main ( -- ) BEGIN nextfib lessthan4mil WHILE even IF dup v @ + v ! ENDIF REPEAT ;
+: nextfib ( -- ) over over + rot drop ;
+: lessthan4mil ( -- ) dup 4000000 < IF true ELSE false ENDIF ;
+: even ( -- ) dup 2 mod 0= IF true ELSE false ENDIF ;
+: add2total ( -- ) dup v @ + v ! ;
+: main ( -- ) BEGIN nextfib lessthan4mil WHILE even IF add2total ENDIF REPEAT ;
 
 1 1 
 main 
